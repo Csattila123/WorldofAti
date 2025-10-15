2474597a9beb0f12b6177e125ef89e546d77fc36
solyom.attila.2022i_ <attilasolyom0@gmail.com>
2025-10-15 13:07:01 +0200

    play and display methods, game loop with scanner

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class Game {
     public static void main(String[] args) {
-        // git remote add origin URL
-        // git remote add origin https://github.com/Csattila123/WorldofAti.git
+        Game game = new Game();
+        game.play();
+
+    }
+
+    private void play() {
+        Scanner scanner = new Scanner(System.in);
+        display("Üdv a várkalandban");
+
+        while (true){
+
+            String input = scanner.nextLine().trim();
+            String command = input;
+
+            switch (command){
+                case "kilep":
+                    display("Köszi a játékot!");
+                    scanner.close();
+                    return;
+            }
+        }
+    }
+
+    private void display(String message) {
+        System.out.println(message);
     }
 }
